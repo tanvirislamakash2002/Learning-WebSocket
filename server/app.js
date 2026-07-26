a5380c949abecb9e4be3b26db9aa1ff06fff7ff5
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-07-26 23:02:56 +0600

    feat: replace boilerplate with socket.io client setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,35 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 
 const port = 3000
 const app = express()
+const server = createServer(app)
 
-app.listen(port, () => {
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        method: ["GET", "POST"],
+        credentials: true
+    }
+})
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    method: ["GET", "POST"],
+    credentials: true
+}))
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+io.on("connection", (socket) => {
+    console.log("user connected")
+    console.log("Id", socket.id)
+})
+
+server.listen(port, () => {
     console.log(`socket server is running on port ${port}`)
 })
