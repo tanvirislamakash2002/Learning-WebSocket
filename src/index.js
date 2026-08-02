e7b4136a4a5e05de34af3b417acaf4a64d844fbf
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-02 22:57:04 +0600

    Replaced main with code from jm-webSocket-course-project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
