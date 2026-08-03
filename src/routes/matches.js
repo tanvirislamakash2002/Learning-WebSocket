e3e91d3ac9516949e6b40064c83748c2d7f454d4
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-03 20:17:19 +0600

    feat(matches): add POST endpoint to create match
    - validates request body with createMatchSchema
    - inserts match with computed status and nullable scores

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -6,3 +6,26 @@ matchRouter.get("/", (req, res) => {
     res.status(200).json({ message: "Matches List" });
 });
 
+matchRouter.post("/", (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+    const { data: { startTime, endTime, homeScore, awayScore } } = parsed
+    if (!parsed.success) {
+        return res.status(400).json({ errors: parsed.error.errors });
+    }
+    try {
+        const [event] = await db.insert(matches).values({
+            ...parsed.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: homeScore ?? 0,
+            awayScore: awayScore ?? 0,
+            status: getMatchStatus(startTime, endTime),
+        }).returning();
+
+        res.status(201).json({ message: "Match created successfully", data: event });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
